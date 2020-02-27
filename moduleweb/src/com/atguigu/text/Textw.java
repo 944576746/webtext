33a4413a2c947420927256bc0982ee1bf49288ed
杨俊 <944576746@qq.com>
2020-02-27 15:09:33 +0800

    Initial commit

---
+++ moduleweb/src/com/atguigu/text/Textw.java
@@ -0,0 +1,10 @@
+package com.atguigu.text;
+
+public class Textw {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 7; i++) {
+            System.out.println(i);
+        }
+    }
+}
